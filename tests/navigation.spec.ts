9cdba9946353b529fba28bbe382b414aa40c6436
Edward Vallejo <edward.vallejo@wiedii.co>
2026-06-28 20:11:15 -0500

    Reto Dia 4

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -43,3 +43,33 @@ test('Check left menu options', async ({ page }) => {
     await expect(firstItem).toHaveText('Admin');
 
 });
+
+test('Navigate thought he left panel', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Verify that the user is logged in by checking for the presence of the dashboard
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    const leftMenuItems = await page.getByLabel('Sidepanel').getByRole('listitem');
+    const currentMenuItemsCount = await leftMenuItems.count();
+
+    for (let i = 0; i < currentMenuItemsCount; i++) {
+        const menuItem = leftMenuItems.nth(i);
+        const menuText = await menuItem.innerText();
+
+        console.log('Current menu item', menuText);
+        if (menuText === 'Maintenance') {
+            await menuItem.click();
+            await page.goBack();
+        }else{
+            await menuItem.click();
+        }
+    }
+
+
+})

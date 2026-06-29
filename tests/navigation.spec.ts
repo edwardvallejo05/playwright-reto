17dad50de0ad65478d87ca9fa874b62a6492c4f5
Edward Vallejo <edward.vallejo@wiedii.co>
2026-06-28 21:35:49 -0500

    Reto Dia 7

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,165 +1,156 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage';
 
 test('Check left menu options', async ({ page }) => {
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123');
 
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-    await page.getByRole('button', { name: 'Login' }).click();
+  // Verify that the user is logged in by checking for the presence of the dashboard
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
 
-    // Verify that the user is logged in by checking for the presence of the dashboard
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+  const leftMenuItems = await page.getByLabel('Sidepanel').getByRole('listitem');
+  const currentMenuItemsCount = await leftMenuItems.count();
+  console.log('CURRENT MENU ITEMS COUNT', currentMenuItemsCount);
 
-    const leftMenuItems = await page.getByLabel('Sidepanel').getByRole('listitem');
-    const currentMenuItemsCount = await leftMenuItems.count();
-    console.log('CURRENT MENU ITEMS COUNT', currentMenuItemsCount);
+  const currentMenuItems: string[] = [];
 
-    const currentMenuItems: string[] = [];
+  for (let i = 0; i < currentMenuItemsCount; i++) {
+    const menuText = await leftMenuItems.nth(i).innerText();
+    currentMenuItems.push(menuText);
+  }
 
-    for (let i = 0; i < currentMenuItemsCount; i++) {
-        const menuText = await leftMenuItems.nth(i).innerText();
-        currentMenuItems.push(menuText);
-    }
+  console.log(currentMenuItems);
 
-    console.log(currentMenuItems);
+  const expectedMenuItems = [
+    'Admin',
+    'PIM',
+    'Leave',
+    'Time',
+    'Recruitment',
+    'My Info',
+    'Performance',
+    'Dashboard',
+    'Directory',
+    'Maintenance',
+    'Claim',
+    'Buzz'
+  ]
 
-    const expectedMenuItems = [
-        'Admin',
-        'PIM',
-        'Leave',
-        'Time',
-        'Recruitment',
-        'My Info',
-        'Performance',
-        'Dashboard',
-        'Directory',
-        'Maintenance',
-        'Claim',
-        'Buzz'
-    ]
-
-    expect(currentMenuItems).toEqual(expectedMenuItems);
-    const firstItem = leftMenuItems.first();
-    await expect(firstItem).toHaveText('Admin');
+  expect(currentMenuItems).toEqual(expectedMenuItems);
+  const firstItem = leftMenuItems.first();
+  await expect(firstItem).toHaveText('Admin');
 
 });
 
 test('Navigate thought he left panel', async ({ page }) => {
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123');
 
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-    await page.getByRole('button', { name: 'Login' }).click();
+  // Verify that the user is logged in by checking for the presence of the dashboard
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
 
-    // Verify that the user is logged in by checking for the presence of the dashboard
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+  const leftMenuItems = await page.getByLabel('Sidepanel').getByRole('listitem');
+  const currentMenuItemsCount = await leftMenuItems.count();
 
-    const leftMenuItems = await page.getByLabel('Sidepanel').getByRole('listitem');
-    const currentMenuItemsCount = await leftMenuItems.count();
+  for (let i = 0; i < currentMenuItemsCount; i++) {
+    const menuItem = leftMenuItems.nth(i);
+    const menuText = await menuItem.innerText();
 
-    for (let i = 0; i < currentMenuItemsCount; i++) {
-        const menuItem = leftMenuItems.nth(i);
-        const menuText = await menuItem.innerText();
+    console.log('Current menu item', menuText);
+    if (menuText === 'Maintenance') {
+      await menuItem.click();
+      await page.goBack();
+    } else {
+      await menuItem.click();
+      await page.waitForLoadState('networkidle')
 
-        console.log('Current menu item', menuText);
-        if (menuText === 'Maintenance') {
-            await menuItem.click();
-            await page.goBack();
-        } else {
-            await menuItem.click();
-            await page.waitForLoadState('networkidle')
-
-        }
     }
+  }
 
 
 })
 
 test('Check all the qualification links', async ({ page }) => {
 
-    const expectedPages = [
-        {
-            menu: 'Skills',
-            url: '/web/index.php/admin/viewSkills'
-        },
-        {
-            menu: 'Education',
-            url: '/web/index.php/admin/viewEducation'
-        },
-        {
-            menu: 'Licenses',
-            url: '/web/index.php/admin/viewLicenses'
-        },
+  const expectedPages = [
+    {
+      menu: 'Skills',
+      url: '/web/index.php/admin/viewSkills'
+    },
+    {
+      menu: 'Education',
+      url: '/web/index.php/admin/viewEducation'
+    },
+    {
+      menu: 'Licenses',
+      url: '/web/index.php/admin/viewLicenses'
+    },
 
-    ]
+  ]
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-    await page.getByRole('button', { name: 'Login' }).click()
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123');
 
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
 
-    await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('link', { name: 'Admin' }).click();
 
-    await page.waitForLoadState('networkidle')
+  await page.waitForLoadState('networkidle')
+
+  await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
+
+  const qualificationOptions = page.getByRole('menu').locator('li')
+
+  for (let expectedPage of expectedPages) {
+
+    const menuOption = qualificationOptions.filter({ hasText: expectedPage.menu })
+    await menuOption.click()
+    await expect(page).toHaveURL(new RegExp(expectedPage.url))
 
     await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
 
-    const qualificationOptions = page.getByRole('menu').locator('li')
-
-    for (let expectedPage of expectedPages) {
-
-        const menuOption = qualificationOptions.filter({ hasText: expectedPage.menu })
-        await menuOption.click()
-        await expect(page).toHaveURL(new RegExp(expectedPage.url))
-
-        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
-
-    }
+  }
 })
 
 test('Check all the Job links', async ({ page }) => {
 
-    const expectedPages = [
-        {
-            menu: 'Job Titles',
-            url: '/web/index.php/admin/viewJobTitleList'
-        },
-        {
-            menu: 'Pay Grades',
-            url: '/web/index.php/admin/viewPayGrades'
-        },
-        {
-            menu: 'Employment Status',
-            url: '/web/index.php/admin/employmentStatus'
-        },
+  const expectedPages = [
+    {
+      menu: 'Job Titles',
+      url: '/web/index.php/admin/viewJobTitleList'
+    },
+    {
+      menu: 'Pay Grades',
+      url: '/web/index.php/admin/viewPayGrades'
+    },
+    {
+      menu: 'Employment Status',
+      url: '/web/index.php/admin/employmentStatus'
+    },
 
-    ]
+  ]
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-    await page.getByRole('button', { name: 'Login' }).click()
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123');
 
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
 
-    await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('link', { name: 'Admin' }).click();
 
-    await page.waitForLoadState('networkidle')
+  await page.waitForLoadState('networkidle')
+
+  await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Job').click()
+
+  const JobOptions = page.getByRole('menu').locator('li')
+
+  for (let expectedPage of expectedPages) {
+
+    const menuOption = JobOptions.filter({ hasText: expectedPage.menu })
+    await menuOption.click()
+    await expect(page).toHaveURL(new RegExp(expectedPage.url))
 
     await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Job').click()
 
-    const JobOptions = page.getByRole('menu').locator('li')
-
-    for (let expectedPage of expectedPages) {
-
-        const menuOption = JobOptions.filter({ hasText: expectedPage.menu })
-        await menuOption.click()
-        await expect(page).toHaveURL(new RegExp(expectedPage.url))
-
-        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Job').click()
-
-    }
+  }
 })
--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
 
 test('Get all the users registered', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-
-  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-  await page.getByRole('button', { name: 'Login' }).click();
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123')
 
   // Verify that the user is logged in by checking for the presence of the dashboard
   await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
@@ -28,18 +27,16 @@ test('Get all the users registered', async ({ page }) => {
     if (username) {
       usernames.push(username);
     }
- 
+
   }
   console.log(usernames);
 
 });
 
 test('Get all the Employee Name registered', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-  await page.getByRole('button', { name: 'Login' }).click();
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123');
 
   // Verify that the user is logged in by checking for the presence of the dashboard
   await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
@@ -62,64 +59,60 @@ test('Get all the Employee Name registered', async ({ page }) => {
     if (employee) {
       employees.push(employee);
     }
- 
+
   }
   console.log(employees);
 });
 
 test('Select specific user for edition', async ({ page }) => {
 
-    // const userForEdition = 'Admin'
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin('Admin', 'admin123');
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-    await page.getByRole('button', { name: 'Login' }).click()
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
 
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+  await page.getByRole('link', { name: 'Admin' }).click()
 
-    await page.getByRole('link', { name: 'Admin' }).click()
+  await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+  await page.getByRole('menuitem', { name: 'Users' }).click();
 
-    await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
-    await page.getByRole('menuitem', { name: 'Users' }).click();
+  const rows = page.getByRole('table').getByRole('row');
 
-    const rows = page.getByRole('table').getByRole('row');
+  const usernames: string[] = [];
 
-    const usernames: string[] = [];
+  const rowCount = await rows.count();
 
-    const rowCount = await rows.count();
+  for (let i = 1; i < rowCount; i++) {
+    const cell = rows.nth(i).getByRole('cell').nth(1);
+    const username = await cell.textContent();
 
-    for (let i = 1; i < rowCount; i++) {
-      const cell = rows.nth(i).getByRole('cell').nth(1);
-      const username = await cell.textContent();
-
-      if (username) {
-        usernames.push(username);
-      }
-  
+    if (username) {
+      usernames.push(username);
     }
-    console.log(usernames);
-    
-    const userForEdition = usernames[Math.floor(Math.random() * usernames.length)];
-    console.log(userForEdition);
 
-    const pencilToEdit = page
-        .getByRole('table')
-        .getByRole('row')
-        .filter({ hasText: userForEdition })
-        .locator('button')
-        .filter({ has: page.locator('i.bi-pencil-fill') })
+  }
+  console.log(usernames);
+
+  const userForEdition = usernames[Math.floor(Math.random() * usernames.length)];
+  console.log(userForEdition);
+
+  const pencilToEdit = page
+    .getByRole('table')
+    .getByRole('row')
+    .filter({ hasText: userForEdition })
+    .locator('button')
+    .filter({ has: page.locator('i.bi-pencil-fill') })
 
 
-    await pencilToEdit.click()
+  await pencilToEdit.click()
 
-    const currentUsername = await page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input")
-        .inputValue()
+  const currentUsername = await page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input")
+    .inputValue()
 
-    expect(currentUsername).toEqual(userForEdition)
+  expect(currentUsername).toEqual(userForEdition)
 
-    expect(page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input"))
-        .toHaveValue(currentUsername)
+  expect(page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input"))
+    .toHaveValue(currentUsername)
 })
 
 
